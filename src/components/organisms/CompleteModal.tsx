efe31cea0df1bff9e8825a9a8cd0c79781a71a76
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-04-19 13:14:39 +0900

    update: react-shareのアプデ 5.2.2 -> 5.3.0(#56)

--- src/components/organisms/CompleteModal.tsx
+++ src/components/organisms/CompleteModal.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import Modal from '@mui/material/Modal';
-import { TwitterShareButton, TwitterIcon } from 'react-share';
+import { XShareButton, XIcon } from 'react-share';
 import SecondaryButton from 'components/atoms/SecondaryButton';
 import { DiffcultyJP } from 'models/Diffculty';
 
@@ -45,13 +45,13 @@ const CompleteModal: FC<Props> = ({
         <h2 id="transition-modal-title">{`${diffculty} ${correctAnswerCount}回正解！`}</h2>
         <p id="transition-modal-description">お疲れさまでしたー</p>
         <Box sx={{ p: 2 }}>
-          <TwitterShareButton
+          <XShareButton
             url="https://h-yoshikawa44.github.io/close-to-2"
             title={`${diffculty} - ${correctAnswerCount}回正解！`}
             hashtags={['close_to_2', 'web1week']}
           >
-            <TwitterIcon size={40} borderRadius={10} />
-          </TwitterShareButton>
+            <XIcon size={40} borderRadius={10} />
+          </XShareButton>
         </Box>
         <Box sx={{ p: 2 }}>
           <SecondaryButton text="再チャレンジ" onClickAction={handleRestart} />
